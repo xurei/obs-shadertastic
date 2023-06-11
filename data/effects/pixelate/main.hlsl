4c3183a8548ad19421e9d59ce5d11b14198473cd
xurei <xureilab@gmail.com>
2023-06-11 04:36:15 +0200

    ✨ Pixelate : clean up code

--- data/effects/pixelate/main.hlsl
+++ data/effects/pixelate/main.hlsl
@@ -1,51 +1,40 @@
-// Custom arguments, referenced in the meta.json file of the effect
 uniform float breaking_point;
 uniform float max_pixelation_level;
-
-#define sampleTex(is_a, uv) ((is_a) ? tex_a.Sample(textureSampler, uv) : tex_b.Sample(textureSampler, uv))
+//----------------------------------------------------------------------------------------------------------------------
 
 sampler_state textureSampler {
-	Filter    = Linear;
-	AddressU  = Clamp;
-	AddressV  = Clamp;
+    Filter    = Linear;
+    AddressU  = Clamp;
+    AddressV  = Clamp;
 };
 
 struct VertData {
-	float2 uv  : TEXCOORD0;
-	float4 pos : POSITION;
+    float2 uv  : TEXCOORD0;
+    float4 pos : POSITION;
 };
 
 struct FragData {
-	float2 uv  : TEXCOORD0;
+    float2 uv  : TEXCOORD0;
 };
 
 VertData VSDefault(VertData v_in)
 {
-	VertData vert_out;
-	vert_out.uv  = v_in.uv;
-	vert_out.pos = mul(float4(v_in.pos.xyz, 1.0), ViewProj);
-	return vert_out;
+    VertData vert_out;
+    vert_out.uv  = v_in.uv;
+    vert_out.pos = mul(float4(v_in.pos.xyz, 1.0), ViewProj);
+    return vert_out;
 }
+//----------------------------------------------------------------------------------------------------------------------
 
 float sigmoid(float strength, float n) {
-	// Vanilla sigmoid ; this is not symetric between 0 and 1
-	//return (1 / (1 + exp(-n)));
+    // Vanilla sigmoid ; this is not symetric between 0 and 1
+    //return (1 / (1 + exp(-n)));
 
-	n = strength * (n*2 - 1);
-	float v0 = (1 / (1 + exp(strength)));
-	float v1 = (1 / (1 + exp(-1 * strength)));
+    n = strength * (n*2 - 1);
+    float v0 = (1 / (1 + exp(strength)));
+    float v1 = 1 - v0; // simplified from v1 = (1 / (1 + exp(-1 * strength)));
 
-	return (1 / (1 + exp(-n)) - v0) / (v1-v0);
-}
-
-float srgb_nonlinear_to_linear_channel(float u)
-{
-	return (u <= 0.04045) ? (u / 12.92) : pow((u + 0.055) / 1.055, 2.4);
-}
-
-float3 srgb_nonlinear_to_linear(float3 v)
-{
-	return float3(srgb_nonlinear_to_linear_channel(v.r), srgb_nonlinear_to_linear_channel(v.g), srgb_nonlinear_to_linear_channel(v.b));
+    return (1 / (1 + exp(-n)) - v0) / (v1-v0);
 }
 
 float4 EffectLinear(float2 uv)
@@ -83,34 +72,35 @@ float4 EffectLinear(float2 uv)
         lerp_t
     );
 }
+//----------------------------------------------------------------------------------------------------------------------
 
 float4 PSEffect(FragData f_in) : TARGET
 {
     float4 rgba = EffectLinear(f_in.uv);
-	rgba.rgb = srgb_nonlinear_to_linear(rgba.rgb);
-	return rgba;
+    rgba.rgb = srgb_nonlinear_to_linear(rgba.rgb);
+    return rgba;
 }
 
 float4 PSEffectLinear(FragData f_in) : TARGET
 {
-	float4 rgba = EffectLinear(f_in.uv);
-	return rgba;
+    float4 rgba = EffectLinear(f_in.uv);
+    return rgba;
 }
 
 technique Effect
 {
-	pass
-	{
-		vertex_shader = VSDefault(v_in);
-		pixel_shader = PSEffect(f_in);
-	}
+    pass
+    {
+        vertex_shader = VSDefault(v_in);
+        pixel_shader = PSEffect(f_in);
+    }
 }
 
 technique EffectLinear
 {
-	pass
-	{
-		vertex_shader = VSDefault(v_in);
-		pixel_shader = PSEffectLinear(f_in);
-	}
+    pass
+    {
+        vertex_shader = VSDefault(v_in);
+        pixel_shader = PSEffectLinear(f_in);
+    }
 }
