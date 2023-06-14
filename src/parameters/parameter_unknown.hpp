3968f481c07534970b3415180d7887f16db3799c
xurei <xureilab@gmail.com>
2023-06-15 00:28:48 +0200

    📝 Add license header in all source files

--- src/parameters/parameter_unknown.hpp
+++ src/parameters/parameter_unknown.hpp
@@ -1,3 +1,20 @@
+/******************************************************************************
+    Copyright (C) 2013-2014 by Hugh Bailey <obs.jim@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 class effect_parameter_unknown : public effect_parameter {
     public:
         effect_parameter_unknown(gs_eparam_t *shader_param) : effect_parameter(sizeof(int), shader_param) {
