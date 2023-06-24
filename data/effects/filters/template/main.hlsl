4a38cf14c9a6810fb32adc07ad772a8c123c8490
xurei <xureilab@gmail.com>
2023-06-24 21:07:27 +0200

    ✨ Added dedicated TEMPLATE effect for filters

--- data/effects/filters/template/main.hlsl
+++ data/effects/filters/template/main.hlsl
@@ -1,8 +1,7 @@
 // Common parameters for all shaders, as reference. Do not uncomment this (but you can remove it safely).
 /*
 uniform float time;            // Time since the shader is running. Goes from 0 to 1 for transition effects; goes from 0 to infinity for filter effects
-uniform texture2d tex_a;       // Texture of the previous frame (transitions only)
-uniform texture2d tex_b;       // Texture of the next frame (transitions only)
+uniform texture2d image;       // Texture of the source (filters only)
 uniform texture2d tex_interm;  // Intermediate texture where the previous step will be rendered (for multistep effects)
 uniform float upixel;          // Width of a pixel in the UV space
 uniform float vpixel;          // Height of a pixel in the UV space
@@ -46,26 +45,15 @@ float4 EffectLinear(float2 uv)
 {
     // -----> YOUR CODE GOES HERE <-----
 
-    // Here is an basic example that will progressively show the next scene from right to left :
-    if (uv[0] < 1-time) {
-        return tex_a.Sample(textureSampler, uv);
-    }
-    else {
-        return tex_b.Sample(textureSampler, uv);
-    }
+    // Here is an basic example that will flip the image
+    uv[0] = 1-uv[0];
+    return image.Sample(textureSampler, uv);
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 // You probably don't want to change anything from this point.
 
 float4 PSEffect(FragData f_in) : TARGET
-{
-    float4 rgba = EffectLinear(f_in.uv);
-    rgba.rgb = srgb_nonlinear_to_linear(rgba.rgb);
-    return rgba;
-}
-
-float4 PSEffectLinear(FragData f_in) : TARGET
 {
     float4 rgba = EffectLinear(f_in.uv);
     return rgba;
@@ -77,14 +65,5 @@ technique Draw
     {
         vertex_shader = VSDefault(v_in);
         pixel_shader = PSEffect(f_in);
-    }
-}
-
-technique DrawLinear
-{
-    pass
-    {
-        vertex_shader = VSDefault(v_in);
-        pixel_shader = PSEffectLinear(f_in);
     }
 }
