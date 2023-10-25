2a82fa4626d3522c9dd9c35989801c273396cd91
xurei <xureilab@gmail.com>
2023-10-25 23:05:55 +0200

    ✨ "Displacement Map (Source)" filter : add "Border method" (Clamp, Mirror, Repeat)

--- data/effects/filters/displacement_map_source/main.hlsl
+++ data/effects/filters/displacement_map_source/main.hlsl
@@ -2,6 +2,7 @@ uniform texture2d displacement_map;
 uniform float displacement_strength_x;
 uniform float displacement_strength_y;
 uniform int color_space;
+uniform int sampler_mode;
 //----------------------------------------------------------------------------------------------------------------------
 
 // These are required objects for the shader to work.
@@ -34,25 +35,61 @@ float3 rgbToYuv(float3 rgb)
 {
     return float3(
         ( 0.257 * rgb.r + 0.504 * rgb.g + 0.098 * rgb.b) / 0.859,
-        (-0.148 * rgb.r - 0.291 * rgb.g + 0.439 * rgb.b),
-        ( 0.439 * rgb.r - 0.368 * rgb.g - 0.071 * rgb.b)
+        (-0.148 * rgb.r - 0.291 * rgb.g + 0.439 * rgb.b) / 0.439,
+        ( 0.439 * rgb.r - 0.368 * rgb.g - 0.071 * rgb.b) / 0.439
     );
 }
 //----------------------------------------------------------------------------------------------------------------------
+
+float applyBorders_single(float val) {
+    if (val > 1.0) {
+        if (sampler_mode == 0) { // Clamp
+            return 1.0;
+        }
+        else if (sampler_mode == 1) { // Mirror
+            return 1.0 - (val - 1.0);
+        }
+        else { // Repeat
+            return val - floor(val);
+        }
+    }
+    else if (val < 0.0) {
+        if (sampler_mode == 0) { // Clamp
+            return 0.0;
+        }
+        else if (sampler_mode == 1) { // Mirror
+            return -1.0 * val;
+        }
+        else { // Repeat
+            return val - floor(val);
+        }
+    }
+    else {
+        return val;
+    }
+}
+float2 applyBorders(float2 uv) {
+    return float2(
+        applyBorders_single(uv[0]),
+        applyBorders_single(uv[1])
+    );
+}
 
 float4 EffectLinear(float2 uv)
 {
     float2 displacement_strength = float2(displacement_strength_x, displacement_strength_y);
     if (color_space == 0) { // RGB
         float4 mask = displacement_map.Sample(textureSampler, uv);
-        float2 uv2 = uv + displacement_strength*float2(mask.x - 0.5, mask.y - 0.5);
+        float2 uv2 = uv - displacement_strength*float2((mask.x - 0.5)/0.5, (mask.y - 0.5)/0.5);
+        uv2 = applyBorders(uv2);
         float4 px = image.Sample(textureSampler, uv2);
         return px;
     }
     else if (color_space == 1) { // YUV
         float4 mask = displacement_map.Sample(textureSampler, uv);
         float3 mask_yuv = rgbToYuv(mask.rgb);
-        float2 uv2 = uv + displacement_strength*float2(mask_yuv.y, mask_yuv.z);
+        float2 uv2 = uv - displacement_strength*float2(mask_yuv.y, mask_yuv.z);
+        uv2 = applyBorders(uv2);
         float4 px = image.Sample(textureSampler, uv2);
         return px;
     }
